13bce03c44469b411ed91ecbeadace94064f8199
nikitagehl <nikkigehlod272@gmail.com>
2024-10-12 19:22:58 +0530

    New files

---
+++ src/Oct/ex02102024/Lab001.java
@@ -0,0 +1,9 @@
+package Oct.ex02102024;
+
+public class Lab001 {
+    public static void main(String[] args) {
+        System.out.println("hello nikita !!!");
+        System.out.println("hello aditi !!!");
+
+    }
+}
---
+++ src/Oct/ex03102024/SimpleCalculator.java
@@ -0,0 +1,10 @@
+package Oct.ex03102024;
+
+public class SimpleCalculator {
+    public static void main(String[] args) {
+        System.out.println( 10+2);
+        System.out.println( 10*2);
+        System.out.println( 10-5);
+        System.out.println( 10/2);
+    }
+}
